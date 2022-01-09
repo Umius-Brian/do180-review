1170a84dd5f780d7663189ff522de33624ae7d01
Brian Bui <umius.brian@gmail.com>
2022-01-09 01:16:36 -0500

    Chore

---
+++ comprehensive.js
@@ -0,0 +1,22 @@
+# Run
+podman run -d --rm -it -v /home/student/local/mysql:/var/lib/mysql/data -p 13306:3306 -e -e -e -e registry.redhat.io/rhel8/mysql-80:1
+
+# MySQL exec (from ~)
+podman exec mysql /bin/bash -c 'mysql -uuser1 -pmypa55 items < /db.sql'
+
+podman exec mysql /bin/bash -c 'mysql -uuser1 -pmypa55 -e "select * from items.Projects;"'
+
+mysql -uuser1 -pmypa55 -h 127.0.0.1 -P13306 items < /home/student/DO180/labs/manage-networking/db.sql
+
+# Verify MySQL 
+podman exec -it mysqldb-port mysql -uroot items -e "select * from Item"
+> Non-interactive
+
+mysql -uuser1 -pmypa55 -h 127.0.0.1 -P13306 items -e "select * from Item"
+> Port-forwarding
+
+podman exec -it mysqldb-port /bin/bash
+bash-4.4$ mysql -uroot items -e "select * from items"
+> Interactive
+
+
--- review.js
+++ review.js
@@ -23,3 +23,29 @@ sudo ps -ef | grep 'sleep 1000'
 
 podman exec -it httpd-basic /bin/bash
 bash-4.4# ls -la /var/www/html
+
+# 7
+podman cp /home/student/DO180/labs/manage-lifecycle/db.sql mysql:/
+podman exec mysql /bin/bash -c 'mysql -uuser1 -pmypa55 items < /db.sql'
+podman exec mysql /bin/bash -c 'mysql -uuser1 -pmypa55 -e "select * from items.Projects;"'
+
+
+# 8
+# First make local directory for volume mount
+mkdir -pv /home/student/local/mysql
+ls -ldZ /home/student/local/mysql
+sudo semanage fcontext -a -t container_file_t '/home/student/local/mysql(/.*)?'
+sudo restorecon -R /home/student/local/mysql
+ls -ldZ /home/student/local/mysql
+podman unshare chown 27:27 /home/student/local/mysql
+podman login registry.redhat.io
+podman pull registry.redhat.io/rhel8/mysql-80:1
+
+podman run --name <app_name> -d -v /home/student/local/mysql:/var/lib/mysql/data -e -e -e -e registry.redhat.io/rhel8/mysql-80:1
+
+ls -ld /home/student/local/mysql/items
+podman unshare ls -ld /home/student/local/mysql/items
+
+
+# 9
+podman run --name mysqldb-port -d -v /home/student/local/mysql:/var/lib/mysql/data -p 13306:3306 -e -e -e -e registry.redhat.io/rhel8/
